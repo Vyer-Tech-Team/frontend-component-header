40785bce1b52871a2f837034fe12bdf1442540a5
ukozazenje <mzecevic04@gmail.com>
2024-06-24 18:08:08 +0200

    testign dist

---
+++ dist/logos.js
@@ -0,0 +1,7 @@
+export var logoUrls = {
+  "footbal": "https://lms-logos.s3.amazonaws.com/football.png",
+  "art": "https://lms-logos.s3.amazonaws.com/art.png",
+  "university": "https://lms-logos.s3.amazonaws.com/university.png",
+  "default": "https://lms-logos.s3.amazonaws.com/Vector.png"
+};
+//# sourceMappingURL=logos.js.map
